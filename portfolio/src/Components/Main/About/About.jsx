8635bf5cbabb42493e9b939fd9bf03252fa5df93
MiguelZ01 <jesusmigu3lzr2004@gmail.com>
2023-07-19 11:48:19 -0500

    Portfolio

--- portfolio/src/Components/Main/About/About.jsx
+++ portfolio/src/Components/Main/About/About.jsx
@@ -10,7 +10,7 @@ export const About = () => {
                 <h4>Desarrollador Web Full Stack apasionado por la creación de experiencias digitales. Con formación en logística industrial, una gran experiencia sobre la vida en el campo, estoy constantemente buscando nuevos retos para mejorar mis habilidades y ofrecer resultados de calidad.</h4>
                 <h4>He adquirido conocimientos en lenguajes como HTML, CSS, JavaScript y Python, al igual que en tecnologías como React, Node.js y bases de datos como PostgreSQL. Además, he aprendido sobre metodologías ágiles, lo que me ha ayudado a saber que tengo la capacidad de afrontar nuevos retos en el ámbito laboral.</h4>
                 <br />
-                <a href="https://drive.google.com/file/d/1ymHn5zpic1KrXakwZJzg6ngYRDMFgttN/view?usp=drive_link" target='_blank' className={styles.button_about_link}>
+                <a href="https://drive.google.com/file/d/1wkop6z206hh6jmE7J0piGC-0Y6J6zVTS/view?usp=sharing" target='_blank' className={styles.button_about_link}>
                     <button className={styles.button_about}>
                         Conocer mi CV
                     </button>
